f6de90979e9fb72403c0da45115c3a01b03450b5
Obinna A. Kalu <obkalu@gmail.com>
2019-08-03 04:10:25 -0500

    Added the web-client functionality

--- server/src/main/java/edu/mum/cs/cs425/demos/elibraryrestapi/controller/BookRestController.java
+++ server/src/main/java/edu/mum/cs/cs425/demos/elibraryrestapi/controller/BookRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:81", allowedHeaders = "*")
+@CrossOrigin(origins = {"http://127.0.0.1:5500","http://localhost:81"}, allowedHeaders = "*")
 public class BookRestController {
 
     private BookService bookService;
